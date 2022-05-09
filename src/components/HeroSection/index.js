e09fb6f8bd122c61b4761ba3c9b80fb3d4e01648
Andras Varga <zoryandras@gmail.com>
2022-05-09 21:11:47 +0200

    New video!

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React,  {useState} from 'react'
-import Video from '../../videos/001.mp4'
+import Video from '../../videos/video.mp4'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 import {Button} from '../ButtonElement'
 
