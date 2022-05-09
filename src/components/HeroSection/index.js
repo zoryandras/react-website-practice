f217dbc60b5ddb9595a58b2d4997893b087a5b92
Andras Varga <zoryandras@gmail.com>
2022-05-09 13:55:12 +0200

    Added HERO and video and buttonelement.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React,  {useState} from 'react'
+import Video from '../../videos/001.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import {Button} from '../ButtonElement'
 
 const HeroSection = () => {
+  const {hover, setHover} = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
   return (
-    <HeroContainer id="home">
-        
+    <HeroContainer>
+        <HeroBg>
+          <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+        </HeroBg>
+        <HeroContent>
+          <HeroH1>
+            Practice makes perfect!
+          </HeroH1>
+          <HeroP>
+            Jinkies!
+          </HeroP>
+          <HeroBtnWrapper>
+            <Button to="signup" 
+            onMouseEnter={onHover} 
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            >
+              Get started {hover ? <ArrowForward /> : <ArrowRight />}
+            </Button>
+          </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
 
-export default HeroSection = 
+export default HeroSection
