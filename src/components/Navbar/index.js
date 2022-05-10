a5fe161cd7201d099f638febbdccc5749a384402
Andras Varga <zoryandras@gmail.com>
2022-05-10 17:39:27 +0200

    afd

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -29,7 +29,7 @@ const Navbar = ({ toggle }) => {
                     </NavItem>
                 </NavMenu>
                 <NavBtn>
-                    <NavBtnLink to="/singin">Sign In!</NavBtnLink>
+                    <NavBtnLink to="/signin">Sign In!</NavBtnLink>
                 </NavBtn>
             </NavbarContainer>
         </Nav>
